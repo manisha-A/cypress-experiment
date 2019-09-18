11a54262e3e2564d49fdfc5517130b7ddfb4f100
Manisha Awasthi <itsmanisha_15@yahoo.co.in>
2019-09-18 21:24:25 +0100

    sample react app with cypress test framework skeleton

---
+++ cypress/integration/example.spec.js
@@ -0,0 +1,9 @@
+context('Actions', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+    });
+
+    it('successfully loads', function() {
+        cy.visit('http://localhost:3000') // change URL to match your dev URL
+    });
+});
